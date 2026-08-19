f65bcbd31fd40c5608ae1abfbd3870a0705fcff6
Jiashu <zjsdut@163.com>
2026-08-19 10:32:30 +0800

    Add

---
+++ lec22/setup/katex.ts
@@ -0,0 +1,15 @@
+import { defineKatexSetup } from '@slidev/types'
+
+export default defineKatexSetup(() => {
+  return {
+    macros: {
+        '\\border': '\\mathrm{border}',
+        '\\pref': '\\mathrm{pref}',
+        '\\lcp': '\\mathrm{lcp}',
+        '\\rev' : '\\overleftarrow{#1}',
+        '\\rad' : '\\operatorname{rad}',
+        '\\lcm' : '\\operatorname{lcm}',
+        "\\multichoose": "\\left(\\kern-.3em\\left(\\genfrac{}{}{0pt}{}{#1}{#2}\\right)\\kern-.3em\\right)",
+    },
+  }
+})
